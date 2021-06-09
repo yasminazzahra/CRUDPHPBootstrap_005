419add5fd0aabb81e659ff0ca2629f3a0b4de83a
yasminazzahra <yasminazzahra.ya@gmail.com>
2021-06-09 19:48:12 +0700

    Update dan Edit

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ $nama = $_POST['nama'];
 $nim = $_POST['nim'];
 $alamat = $_POST['alamat'];
 
-mysqli_query($koneksi, "update mahasiwa set nama='$nama', nim='$nim, alamat='$alamat' where id='$id'");
+mysqli_query($koneksi, "update mahasiwa set nama='$nama', nim='$nim', alamat='$alamat' where id='$id'");
 
 header("location:index.php");
 ?>
